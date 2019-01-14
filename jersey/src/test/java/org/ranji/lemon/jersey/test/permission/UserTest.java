56fdc4e20efebc958d2ec7282be71df9e2e89d5d
jiran1221 <jiran1221@163.com>
2019-01-14 23:14:20 +0800

    实现User的增删查改功能

--- jersey/src/test/java/org/ranji/lemon/jersey/test/permission/UserTest.java
+++ jersey/src/test/java/org/ranji/lemon/jersey/test/permission/UserTest.java
@@ -87,7 +87,7 @@ public class UserTest {
 	
 	@Test
 	public void testInsert(){
-		for(int i=0; i<10; i++){
+		for(int i=0; i<20; i++){
 			User u = new User();
 			u.setUsername("zhangsan"+i+1);
 			u.setPassword("123"+i);
